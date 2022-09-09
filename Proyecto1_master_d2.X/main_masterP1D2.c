44e3baf668869b4442ac0d16e45f7a32b7694cb9
ANDYREWAS <99231727+ANDYREWAS@users.noreply.github.com>
2022-09-09 09:36:18 -0600

    Comentario main master

--- Proyecto1_master_d2.X/main_masterP1D2.c
+++ Proyecto1_master_d2.X/main_masterP1D2.c
@@ -4,12 +4,14 @@
  *
  * Created on 9 de septiembre de 2022, 08:53 AM
  */
+
+//Conectar Master con LCD y con ESP por medio de SPI ademas de conectar a pic esclavo por UART
 #include "LCD.h"
 #include "Osciladors.h"
 #include "SPI.h"
-
 #include <xc.h>
 
 void main(void) {
+    
     return;
 }
